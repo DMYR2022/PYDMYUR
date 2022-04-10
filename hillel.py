a4ef2d94a47afa43023540ac974e9508e8791aa8
DMYR2022 <103372844+DMYR2022@users.noreply.github.com>
2022-04-10 20:28:13 +0300

    Add files via upload

---
+++ hillel.py
@@ -0,0 +1,24 @@
+"""first=10
+second=20
+third=first+second
+print(third)
+third=first*second
+print(third)
+third=second/first
+print(third)
+third=first**second
+print(third)
+c=15
+res=first<c
+print(res)
+res=first>c
+print(res)
+res=first==c
+print(res)
+res=c>second
+print(res)"""
+
+str_1 = "Hello"
+str_2= " World"
+str=str_1+str_2
+print(str)
